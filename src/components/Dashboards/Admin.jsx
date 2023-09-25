b43c7ece76e7ba8f920aaa95ed3219cd773e0f0e
Evans <ellaboevans@gmail.com>
2023-09-25 20:11:36 +0000

    fix(app): Icon labels in attr
    Renamed the Icon Label in the src attribute to the exact file name in the asset directory. All good at my end now.

--- src/components/Dashboards/Admin.jsx
+++ src/components/Dashboards/Admin.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
-import Layout from './Layout';
+import { Outlet } from 'react-router-dom'
+import Layout from './Layout'
 
 const Admin = () => {
-    const userMenus = [
-        { title: "Admin", src: "user", gap: true,  path: "/" },
-        { title: "Dashboard", src: "Chat" , path: "/" },
-        { title: "Lessons",  src: "chart", path: "/Lessons"  },
-        { title: "Users", src: "Chat" , path: "/" },
-        { title: "Create Assignment",  src: "chart", path: "/Lessons"  },
-        { title: "Assignment", src: "Calendar",  path: "/" },   
-      ];
+  const userMenus = [
+    { title: 'Admin', src: 'User', gap: true, path: '/' },
+    { title: 'Dashboard', src: 'Chat', path: '/' },
+    { title: 'Lessons', src: 'Chart', path: '/Lessons' },
+    { title: 'Users', src: 'Chat', path: '/' },
+    { title: 'Create Assignment', src: 'Chart', path: '/Lessons' },
+    { title: 'Assignment', src: 'Calendar', path: '/' }
+  ]
 
   return (
     <Layout menus={userMenus}>
-        <Outlet/>
+      <Outlet />
     </Layout>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,77 +1,72 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-
-
-const Sidebar = ({Menus}) => {
-  const [open, setOpen] = useState(window.innerWidth >= 768); 
+const Sidebar = ({ Menus }) => {
+  const [open, setOpen] = useState(window.innerWidth >= 768)
 
   useEffect(() => {
     const handleResize = () => {
-      setOpen(window.innerWidth >= 768);
-    };
+      setOpen(window.innerWidth >= 768)
+    }
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const handleMenuItemClick = () => {
     if (window.innerWidth < 768) {
-      setOpen(false);
+      setOpen(false)
     }
-  };
-
+  }
 
   return (
     <div className=" text-white">
       <div
         className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-[#000] h-screen p-5 pt-8 flex flex-col justify-between relative duration-300` }
+          open ? 'w-72' : 'w-20 '
+        } bg-[#000] h-screen p-5 pt-8 flex flex-col justify-between relative duration-300`}
       >
         <img
           src="/src/assets/control.png"
           className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
+           border-2 rounded-full  ${!open && 'rotate-180'}`}
           onClick={() => setOpen(!open)}
         />
         <ul className="pt-6">
           {Menus.map((Menu, index) => (
-            <Link to={`/Dashboard${Menu.path}`}
+            <Link
+              to={`/Dashboard${Menu.path}`}
               key={index}
               className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mb-[28px]" : "mt-2"}  `}
+              ${Menu.gap ? 'mb-[28px]' : 'mt-2'}  `}
               onClick={handleMenuItemClick}
             >
               <img src={`/src/assets/${Menu.src}.png`} />
-              <span  className={`${!open && "hidden"} origin-left duration-200`}>
+              <span className={`${!open && 'hidden'} origin-left duration-200`}>
                 {Menu.title}
               </span>
             </Link>
-            
           ))}
-                    
         </ul>
         <div className="mb-8  ">
           <div className="flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 items-center gap-x-4 float">
-              <img src='/src/assets/Setting.png' />
-                <span  className={`${!open && "hidden"} origin-left duration-200`}>
-                  Settings
-                </span>
+            <img src="/src/assets/Setting.png" />
+            <span className={`${!open && 'hidden'} origin-left duration-200`}>
+              Settings
+            </span>
           </div>
           <div className="flex rounded-md p-2 cursor-pointer text-gray-300 hover:bg-light-white items-center gap-x-4 float">
-              <img src='/src/assets/user.png' />
-                <span  className={`${!open && "hidden"} origin-left duration-200`}>
-                  Logout
-                </span>
+            <img src="/src/assets/User.png" />
+            <span className={`${!open && 'hidden'} origin-left duration-200`}>
+              Logout
+            </span>
           </div>
         </div>
-      </div>       
+      </div>
     </div>
-  );
-};
-export default Sidebar;
-
+  )
+}
+export default Sidebar
