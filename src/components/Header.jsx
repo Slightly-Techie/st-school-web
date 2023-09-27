ae58ee22bf4556d0bf9e9221876fcdd58ec6532c
Evans <ellaboevans@gmail.com>
2023-09-27 16:55:19 +0000

    fix(app): Fix header margin top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import circled from './../assets/circled.png'
 
 const Header = ({ title, para, stream, duration }) => {
   return (
-    <header className="flex flex-col md:flex-row md:items-center mt-10 md:mt-0 justify-between border-b py-3 md:pb-4 md:gap-0 gap-4">
+    <header className="flex flex-col md:flex-row md:items-center mt-10 justify-between border-b py-3 md:pb-4 md:gap-0 gap-4">
       <div>
         <h1 className="font-bold text-3xl">{title}</h1>
         <p className="text-[#444444]">{para}</p>
