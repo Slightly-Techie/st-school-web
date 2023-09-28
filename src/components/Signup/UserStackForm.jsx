d5e87dbabe5bef0e3381aae22f47b1e4c0ef869f
Evans <ellaboevans@gmail.com>
2023-09-28 11:37:50 +0000

    refactor(app): Defaulted the Tech Stack Field

--- src/components/Signup/UserStackForm.jsx
+++ src/components/Signup/UserStackForm.jsx
@@ -1,36 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import LayoutWrapper from "./components/LayoutWrapper";
-import { useState } from "react";
+import { Link } from 'react-router-dom'
+import LayoutWrapper from './components/LayoutWrapper'
+import { useState } from 'react'
 
-const UserStackForm = ({onNext, onPrevious}) => {
+const UserStackForm = ({ onNext, onPrevious }) => {
+  const [techStack, setTechStack] = useState('')
+  const [languageOptions, setLanguageOptions] = useState([])
 
-    const [techStack, setTechStack] = useState("");
-    const [languageOptions, setLanguageOptions] = useState([]);
-  
-    const handleTechStackChange = (e) => {
-      const selectedTechStack = e.target.value;
-      setTechStack(selectedTechStack);
-  
-      if (selectedTechStack === "frontend") {
-        setLanguageOptions(["JavaScript-react","JavaScript-angular","Python-django"]);
-      } else if (selectedTechStack === "backend") {
-        setLanguageOptions(["Ruby", "Python", "Java"]);
-      } else if (selectedTechStack === "Mobile") {
-        setLanguageOptions(["React-Native", "Flutter", "Swift"]);
-      }
-    };
+  const handleTechStackChange = (e) => {
+    const selectedTechStack = e.target.value
+    setTechStack(selectedTechStack)
+
+    if (selectedTechStack === 'frontend') {
+      setLanguageOptions([
+        'JavaScript-react',
+        'JavaScript-angular',
+        'Python-django'
+      ])
+    } else if (selectedTechStack === 'backend') {
+      setLanguageOptions(['Ruby', 'Python', 'Java'])
+    } else if (selectedTechStack === 'Mobile') {
+      setLanguageOptions(['React-Native', 'Flutter', 'Swift'])
+    }
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    onNext();
-  };
+    e.preventDefault()
+    onNext()
+  }
 
   const handleNavigatePrevious = (e) => {
     e.preventDefault()
     onPrevious()
   }
-  
 
   return (
     <LayoutWrapper>
@@ -43,19 +44,21 @@ const UserStackForm = ({onNext, onPrevious}) => {
             Tech Stack
           </label>
           <select
-          type="text"
-          id="techStack"
-          name="techStack"
-          className="border border-[#C9C9C9] focus:outline-gray-600 w-full p-2 rounded-lg"
-          required
-          value={techStack}
-          onChange={handleTechStackChange}
-        >
-          <option value=""></option>
-          <option value="frontend">Frontend</option>
-          <option value="backend">Backend</option>
-          <option value="Mobile">Mobile</option>
-        </select>
+            type="text"
+            id="techStack"
+            name="techStack"
+            className="border border-[#C9C9C9] focus:outline-gray-600 w-full p-2 rounded-lg"
+            required
+            value={techStack}
+            onChange={handleTechStackChange}
+          >
+            <option value="" disabled>
+              Please select your stack
+            </option>
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+            <option value="Mobile">Mobile</option>
+          </select>
         </div>
         <div className="mb-7">
           <label
@@ -65,18 +68,22 @@ const UserStackForm = ({onNext, onPrevious}) => {
             Language
           </label>
           <select
-          type="text"
-          id="language"
-          name="language"
-          className="border border-[#050505] cursor-pointer focus:outline-gray-600 w-full p-2 rounded-lg"
-          required
-        >
-          {languageOptions.map((language) => (
-            <option className="cursor-pointer" key={language} value={language.toLowerCase()}>
-              {language}
-            </option>
-          ))}
-        </select>
+            type="text"
+            id="language"
+            name="language"
+            className="border border-[#050505] cursor-pointer focus:outline-gray-600 w-full p-2 rounded-lg"
+            required
+          >
+            {languageOptions.map((language) => (
+              <option
+                className="cursor-pointer"
+                key={language}
+                value={language.toLowerCase()}
+              >
+                {language}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="flex md:flex-row items-center flex-col gap-2">
           <button
@@ -97,14 +104,14 @@ const UserStackForm = ({onNext, onPrevious}) => {
         <div className="pb-3 md:mt-4">
           <p>
             <span>Already have an account?</span>
-            <Link to={"/login"} className="font-semibold ml-2">
+            <Link to={'/login'} className="font-semibold ml-2">
               Sign in
             </Link>
           </p>
         </div>
       </form>
     </LayoutWrapper>
-  );
-};
+  )
+}
 
-export default UserStackForm;
+export default UserStackForm
