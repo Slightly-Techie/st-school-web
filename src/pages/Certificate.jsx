c11902d59f0bf6690f477cb2c1443ed7115c066a
Evans <ellaboevans@gmail.com>
2023-09-29 02:40:00 +0000

    feat(app): High level component for the Certificates
    This is where we have all the put

---
+++ src/pages/Certificate.jsx
@@ -0,0 +1,11 @@
+import CertificateTemplate from '../components/Certificate/CertificateTemplate'
+
+const Certificate = () => {
+  return (
+    <>
+      <CertificateTemplate />
+    </>
+  )
+}
+
+export default Certificate
