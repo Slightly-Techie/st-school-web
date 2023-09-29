012e921086b88461c96422c9d42c95ed7b216a94
Evans <ellaboevans@gmail.com>
2023-09-29 11:04:24 +0000

    fix(app): Fix Button Position

--- src/components/Certificate/CertificateTemplate.jsx
+++ src/components/Certificate/CertificateTemplate.jsx
@@ -5,10 +5,10 @@ const CertificateTemplate = () => {
   const { pdfRef, getPdf, loader } = useCertificateGenerator()
   return (
     <>
-      <main className="py-[3rem] px-[4rem] relative" ref={pdfRef}>
+      <main className="py-[3rem] px-[4rem] overflow-x-hidden" ref={pdfRef}>
         <CertificateInfo />
       </main>
-      <div className="flex items-center justify-center pt-6 absolute top-[40rem] left-[30rem] mb-6">
+      <div className="flex items-center justify-center mx-auto pt-6 -mt-14 py-3 mb-6 overflow-x-hidden">
         <button
           className="text-center px-6 py-3 bg-black hover:bg-[#434343] font-bold transition-all duration-100 text-white"
           onClick={getPdf}
