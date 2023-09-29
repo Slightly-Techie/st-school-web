621272b99734e69eb31e19a68eba9657ea3e74a7
Evans <ellaboevans@gmail.com>
2023-09-29 15:08:11 +0000

    fix(app): Virtual Hierarchy
    Improved the border weight and the virtual hierarchy for the typographies

--- src/components/Certificate/CertificateInfo.jsx
+++ src/components/Certificate/CertificateInfo.jsx
@@ -4,21 +4,21 @@ import signature from '../../assets/Signature.png'
 const CertificateInfo = () => {
   return (
     <>
-      <div className="container border-[35px] py-4 border-[#434343] overflow-x-hidden">
+      <div className="container border-[50px] py-4 border-[#434343] overflow-x-hidden">
         <div className="flex flex-col items-center justify-center pt-5">
           <img src={logo} alt="" className="w-16 h-12" />
           <h2 className="text-xl py-3">
             Slightly <span className="font-bold">Techie School</span>
           </h2>
-          <h1 className="text-4xl font-bold mt-1 text-[#434343]">
+          <h1 className="text-4xl font-extrabold mt-1 text-[#434343]">
             CERTIFICATE OF COURSE COMPLETION
           </h1>
           <h3 className="text-xl pt-1 pb-2">Awarded To</h3>
-          <h1 className="text-4xl font-bold mt-2 text-[#434343]">
+          <h1 className="text-3xl font-bold mt-2 text-[#434343]">
             PROF. NANA KWESI ASANTE
           </h1>
           <h3 className="text-xl pt-1 pb-2">having successfully completed</h3>
-          <h1 className="text-3xl font-bold mt-1 text-[#434343]">
+          <h1 className="text-2xl font-bold mt-4 text-[#434343]">
             Basics of Frontend Development
           </h1>
           <h3 className="text-xl pt-1 pb-">on 28th September, 2023</h3>
