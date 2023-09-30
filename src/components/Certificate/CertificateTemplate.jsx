a79a51022ca3d44fc811f6c65574683ed2938911
Evans <ellaboevans@gmail.com>
2023-09-30 00:53:14 +0000

    fix(app): Fixed Template Padding Horizontal

--- src/components/Certificate/CertificateTemplate.jsx
+++ src/components/Certificate/CertificateTemplate.jsx
@@ -6,7 +6,10 @@ const CertificateTemplate = () => {
   const { pdfRef, getPdf, loader } = useCertificateGenerator()
   return (
     <>
-      <main className="py-[3rem] px-[4rem] overflow-x-hidden" ref={pdfRef}>
+      <main
+        className="container mx-auto py-[3rem] px-[2.5rem] overflow-x-hidden"
+        ref={pdfRef}
+      >
         <CertificateInfo />
       </main>
       <div className="flex items-center justify-center mx-auto pt-6 -mt-14 py-3 mb-6 overflow-x-hidden">
@@ -14,7 +17,13 @@ const CertificateTemplate = () => {
           className="text-center px-6 py-3 bg-black hover:bg-[#434343] font-bold transition-all duration-100 text-white"
           onClick={getPdf}
         >
-          {loader ? <span>Downloading... <Spinner/> </span> : <span>Download</span>}
+          {loader ? (
+            <span>
+              Downloading... <Spinner />{' '}
+            </span>
+          ) : (
+            <span>Download</span>
+          )}
         </button>
       </div>
     </>
