7a16bcac7ae1b4d960fb7b1215dbee564d821dc3
Evans <ellaboevans@gmail.com>
2023-09-30 03:28:55 +0000

    feat(app): Signature Placement

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -2,9 +2,9 @@ import CertificateTemplate from '../components/Certificate/CertificateTemplate'
 
 const Certificate = () => {
   return (
-    <>
+    <div className="container flex justify-center mx-auto">
       <CertificateTemplate />
-    </>
+    </div>
   )
 }
 
